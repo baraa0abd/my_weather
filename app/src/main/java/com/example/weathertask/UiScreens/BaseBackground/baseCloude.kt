27dbb6ffe1a9f97eb14bce9f589599b629bf0679
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-06-12 03:13:17 +0300

    Refactor: Adjust `baseCloude` Composable offset
    The horizontal offset for the `baseCloude` Composable has been changed from `89.94.dp` to `82.94.dp`.
    
    Additionally, a transparent border has been added to the `Box` modifier, and `Modifier.fillMaxSize()` has been removed from the `Image`.

--- app/src/main/java/com/example/weathertask/UiScreens/BaseBackground/baseCloude.kt
+++ app/src/main/java/com/example/weathertask/UiScreens/BaseBackground/baseCloude.kt
@@ -2,6 +2,7 @@ package com.example.weathertask.UiScreens.BaseBackground
 
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
@@ -16,13 +17,13 @@ import com.example.weathertask.R
 fun baseCloude(){
     Box(
         modifier = Modifier
-            .offset(89.94.dp,166.53.dp)
+            .offset(82.94.dp,166.53.dp)
             .size(197.27.dp,133.47.dp)
+            .border(1.dp, androidx.compose.ui.graphics.Color.Transparent)
     ){
         Image(
             painter = painterResource(id = R.drawable.cloud),
             contentDescription = "Cloud",
-            modifier = Modifier.fillMaxSize()
         )
     }
 }
